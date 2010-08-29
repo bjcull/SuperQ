038b2f33367fe892317d0989a58ae78efea4c156
Luke Lowrey <lukenlowrey@gmail.com>
2010-08-29 21:27:08 +1000

    hacky hack hacked in some db code

--- Test/SuperQTest.cs
+++ Test/SuperQTest.cs
@@ -3,7 +3,6 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SuperQ;
 
 namespace Test
 {
@@ -15,57 +14,58 @@ namespace Test
     {
         public SuperQTest()
         {
-            //
-            // TODO: Add constructor logic here
-            //
         }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [TestMethod]
         public void GetQueueTest()
         {
-            SuperQ.SuperQ testQueue = SuperQ.SuperQ.GetQueue("Queue");
+            var testQueue = SuperQ.SuperQ.GetQueue("Queue");
 
             Assert.AreNotEqual(null, testQueue);
         }
+
+        [TestMethod]
+        public void DeleteQueue()
+        {
+            var testQueue = SuperQ.SuperQ.GetQueue("Queue");
+            //testQueue.Delete();
+
+            //todo asset not exists
+        }
+
+
+        [TestMethod]
+        public void PushMessage()
+        {
+            var message = new SuperQ.QueueMessage<TestClass>()
+            {
+                Payload = new TestClass
+                {
+                    word = "anthony",
+                    number = 7
+                }
+            };
+
+            var testQueue = SuperQ.SuperQ.GetQueue("Queue");
+            testQueue.PushMessage<TestClass>(message);
+        }
+
+
+        [TestMethod]
+        public void GetMessage()
+        {
+            var testQueue = SuperQ.SuperQ.GetQueue("Queue");
+            var message = testQueue.GetMessage<TestClass>();
+
+            Assert.AreEqual("anthony", message.Payload.word);
+            Assert.AreEqual(7, message.Payload.number);
+        }
+
+    }
+
+    public class TestClass
+    {
+        public string word { get; set; }
+        public int number { get; set; }
     }
 }
